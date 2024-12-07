46f8c41ca6013cafeab04b2fe9a4461abef2cc75
ryalfarissi <fahrialfarissi2004@gmail.com>
2024-12-07 13:14:51 +0700

    first commit

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'build', // Set the output directory to 'build'
-  },
+  base: "/nikeapp "
 })
+
